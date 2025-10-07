36775194c950bf9ae5e518c7a07fe0aae8300496
cutiep4nda <haidaragung16@gmail.com>
2025-10-07 15:43:02 +0700

    fix missing code after rebase on rapor.tsx

--- resources/js/pages/rapor.tsx
+++ resources/js/pages/rapor.tsx
@@ -1,14 +1,17 @@
-import { Link } from '@inertiajs/react';
+import { Link, router, usePage } from '@inertiajs/react';
 import Footer from './footer';
 
 export default function Rapor() {
+    const { props }: any = usePage();
+    const { nama, nim, email, kode, birdept } = props;
+
     return (
         <div>
             <div className="bg-gradient-to-b from-primary5 via-primary4 to-primary5 font-poppins text-white">
                 <div className="container mx-auto flex min-h-screen max-w-5xl flex-col items-center justify-center gap-4 px-4 py-16 md:gap-12">
                     <div className="flex flex-col text-center">
-                        <p className="text-4xl font-bold">Hello, Azka Raditya Hafidz</p>
-                        <p>Biro Riset dan Pengembangan</p>
+                        <p className="text-4xl font-bold">Hello, {nama}</p>
+                        <p>{birdept[1]}</p>
                     </div>
 
                     <div className="flex max-w-5xl items-center justify-center gap-4 md:gap-12">
@@ -16,7 +19,11 @@ export default function Rapor() {
                             <img src="/logo/Home1.png" alt="logo1" className="aspect-square h-32 w-32 scale-125 object-contain md:h-44 md:w-44" />
                         </div>
                         <div className="rounded-full border-2 border-primary4 bg-white ring-2 ring-white transition duration-700 hover:scale-110">
-                            <img src="/logo/ekraf.png" alt="logo2" className="aspect-square h-32 w-32 scale-90 object-contain md:h-44 md:w-44" />
+                            <img
+                                src={`/logo/${birdept[0]}.png`}
+                                alt="logo2"
+                                className="aspect-square h-32 w-32 scale-90 object-contain md:h-44 md:w-44"
+                            />
                         </div>
                     </div>
 
@@ -39,7 +46,20 @@ export default function Rapor() {
                         >
                             Rapor 1
                         </Link>
+                        <Link
+                            href="/raport/2"
+                            className="flex w-full max-w-xl justify-center rounded-2xl border-2 border-primary4 bg-white py-3 font-semibold text-primary4 shadow-md transition duration-500 hover:bg-primary4 hover:text-white hover:shadow-lg"
+                        >
+                            Rapor 2
+                        </Link>
+                        <Link
+                            href="/raport/3"
+                            className="flex w-full max-w-xl justify-center rounded-2xl border-2 border-primary4 bg-white py-3 font-semibold text-primary4 shadow-md transition duration-500 hover:bg-primary4 hover:text-white hover:shadow-lg"
+                        >
+                            Rapor 3
+                        </Link>
                     </div>
+                    <button onClick={() => router.post('/logout')}>Logout</button>
                 </div>
             </div>
             <Footer></Footer>
